ed8ccceb587bca4a31ca7b1b03f394247ea01f62
yordanov0502 <yordanovtodor281@gmail.com>
2024-08-14 13:17:07 +0300

    [T18] New method added to rest export

--- restexport/src/main/java/com/tinqinacademy/hotel/restexport/HotelRestExport.java
+++ restexport/src/main/java/com/tinqinacademy/hotel/restexport/HotelRestExport.java
@@ -4,6 +4,7 @@ package com.tinqinacademy.hotel.restexport;
 import com.tinqinacademy.hotel.api.RestApiRoutes;
 import com.tinqinacademy.hotel.api.operations.hotel.bookroom.BookRoomInput;
 import com.tinqinacademy.hotel.api.operations.hotel.bookroom.BookRoomOutput;
+import com.tinqinacademy.hotel.api.operations.hotel.getroom.RoomInfoOutput;
 import com.tinqinacademy.hotel.api.operations.hotel.unbookroom.UnbookRoomInput;
 import com.tinqinacademy.hotel.api.operations.hotel.unbookroom.UnbookRoomOutput;
 import feign.Headers;
@@ -15,8 +16,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Headers({"Content-Type: application/json"})
 public interface HotelRestExport {
 
-//    @RequestLine("GET "+RestApiRoutes.GET_INFO_FOR_ROOM)
-//    RoomInfoOutput getRoomById(@Param String roomId);
+    @RequestLine("GET "+RestApiRoutes.GET_INFO_FOR_ROOM)
+    RoomInfoOutput getRoomById(@Param String roomId);
 
     @RequestLine("POST "+RestApiRoutes.BOOK_ROOM)
     BookRoomOutput bookRoom(@Param String roomId, @RequestBody BookRoomInput input);
