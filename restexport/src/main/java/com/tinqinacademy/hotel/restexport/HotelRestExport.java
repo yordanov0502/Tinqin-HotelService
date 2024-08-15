f4a5f03fa243be5e95aed718614f4baf7f548d1d
Todor Yordanov <yordanovtodor281@gmail.com>
2024-08-15 12:47:57 +0300

    [T19] Method for fetching all free rooms added to rest export (#42)

--- restexport/src/main/java/com/tinqinacademy/hotel/restexport/HotelRestExport.java
+++ restexport/src/main/java/com/tinqinacademy/hotel/restexport/HotelRestExport.java
@@ -4,6 +4,7 @@ package com.tinqinacademy.hotel.restexport;
 import com.tinqinacademy.hotel.api.RestApiRoutes;
 import com.tinqinacademy.hotel.api.operations.hotel.bookroom.BookRoomInput;
 import com.tinqinacademy.hotel.api.operations.hotel.bookroom.BookRoomOutput;
+import com.tinqinacademy.hotel.api.operations.hotel.getavailablerooms.AvailableRoomsIdsOutput;
 import com.tinqinacademy.hotel.api.operations.hotel.getroom.RoomInfoOutput;
 import com.tinqinacademy.hotel.api.operations.hotel.unbookroom.UnbookRoomInput;
 import com.tinqinacademy.hotel.api.operations.hotel.unbookroom.UnbookRoomOutput;
@@ -12,9 +13,19 @@ import feign.Param;
 import feign.RequestLine;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.time.LocalDate;
+
 
 @Headers({"Content-Type: application/json"})
 public interface HotelRestExport {
+
+    @RequestLine("GET /api/v1/hotel/rooms?startDate={startDate}&endDate={endDate}&bedCount={bedCount}&bedSize={bedSize}&bathroomType={bathroomType}")
+    AvailableRoomsIdsOutput getAvailableRooms(
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate,
+            @Param("bedCount") Integer bedCount,
+            @Param("bedSize") String bedSize,
+            @Param("bathroomType") String bathroomType);
 
     @RequestLine("GET "+RestApiRoutes.GET_INFO_FOR_ROOM)
     RoomInfoOutput getRoomById(@Param String roomId);
